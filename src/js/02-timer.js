1c0c410bfc8c4d5f6c903eedc29ccafa05e47861
Oskar Sadkowski <117320665+sadkooski@users.noreply.github.com>
2023-05-15 22:41:41 +0200

    1

---
+++ src/js/02-timer.js
@@ -0,0 +1,73 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const input = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('[data-start]');
+const daysSpan = document.querySelector('[data-days]');
+const hoursSpan = document.querySelector('[data-hours]');
+const minutesSpan = document.querySelector('[data-minutes]');
+const secondsSpan = document.querySelector('[data-seconds]');
+
+const date = new Date();
+let leftTime = 0;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0].getTime() < date.getTime()) {
+      window.alert('Please choose a date in the future');
+    } else {
+      startBtn.removeAttribute('disabled');
+      leftTime = selectedDates[0].getTime() - date.getTime();
+
+      console.log(leftTime);
+    }
+  },
+};
+
+function addLeadingZero(value) {
+  // if (value.length < 2)
+  const newValue = value;
+  newValue = newValue.padStart(1, '0');
+  console.log(newValue);
+  console.log('works');
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  addLeadingZero(days);
+
+  daysSpan.textContent = days;
+  hoursSpan.textContent = hours;
+  minutesSpan.textContent = minutes;
+  secondsSpan.textContent = seconds;
+
+  return console.log({ days, hours, minutes, seconds });
+}
+
+startBtn.setAttribute('disabled', 'true');
+startBtn.addEventListener('click', () => {
+  setInterval(() => {
+    leftTime -= 1000;
+    console.log(leftTime);
+    convertMs(leftTime);
+  }, 1000);
+});
+flatpickr(input, options);
