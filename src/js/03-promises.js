31c5e75127f537a0e5f37f2065da185f53bcd879
Oskar Sadkowski <117320665+sadkooski@users.noreply.github.com>
2023-05-19 21:58:41 +0200

    3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,5 @@
+import Notiflix from 'notiflix';
+
 const inputDelay = document.querySelector('[name="delay"]');
 const inputStep = document.querySelector('[name="step"]');
 const inputAmount = document.querySelector('[name="amount"]');
@@ -10,11 +12,12 @@ let step;
 let amount;
 
 form.addEventListener('input', inputsListener);
-form.addEventListener('submit', createPromiseMultiple);
+form.addEventListener('submit', createPromiseMultipleTimes);
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
 
+  //
   return new Promise((resolve, reject) => {
     if (shouldResolve) {
       resolve({ position, delay });
@@ -24,23 +27,31 @@ function createPromise(position, delay) {
       // Reject
     }
   });
+  //
 }
 
-function createPromiseMultiple(event) {
+function createPromiseMultipleTimes(event) {
   event.preventDefault();
   setTimeout(() => {
+    console.log(`delay ${delay}`);
+
     for (let i = 0; i < amount; i++) {
+      // setTimeout(() => {
       position = i + 1;
-      setTimeout(() => {
-        // mejbi set Interval
-        createPromise(position, delay)
-          .then(({ position, delay }) => {
-            console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-          })
-          .catch(({ position, delay }) => {
-            console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-          });
-      }, step);
+      createPromise(position, delay)
+        .then(({ position, delay }) => {
+          console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+          Notiflix.Notify.success(
+            `✅ Fulfilled promise ${position} in ${delay}ms`
+          );
+        })
+        .catch(({ position, delay }) => {
+          console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+          Notiflix.Notify.failure(
+            `❌ Rejected promise ${position} in ${delay}ms`
+          );
+        });
+      // }, step);
     }
   }, delay);
 }
